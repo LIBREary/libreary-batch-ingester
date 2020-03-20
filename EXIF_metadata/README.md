b9a0ee629b4c5dfee1bd97196a05fd64250ea81a
Ben Glick <glick@glick.cloud>
2020-03-20 13:04:01 -0700

    add EXIF example

---
+++ EXIF_metadata/README.md
@@ -0,0 +1,19 @@
+# EXIF metadata configuration
+
+In this scenario, which I believe is relatively frequently occurring, data is stored in a single folder containing images. Each of these images can have some EXIF metadata we want to keep. 
+
+That looks something like this:
+
+In the directory is:
+
+- images
+	- file_1.jpg
+	- file_1.tif
+	- file_2.jpg
+	- file_2.tif
+
+In this case, we're going to use the metadata tag "collection" to refer to the fact that we're using the "exif" strategy.
+
+
+
+
---
+++ EXIF_metadata/batch_ingest_EXIF.py
@@ -0,0 +1,66 @@
+"""
+Example script to ingest a bunch of files in a set of directories.
+
+This script assumes it's only one level deep, but it could be easily modified 
+    to not make that assumption
+
+Each file has an optional JSON metadata file in the same directory as it which contains
+    its metadata. This assumption could also be easily modified
+"""
+from libreary import Libreary
+import json
+import os
+
+# Get Libreary object
+la = Libreary("../run_dir/config")
+
+# Go through directories
+subdirs = [d for d in os.listdir(".") if os.path.isdir(d)]
+for directory in subdirs:
+    # Ignore second-level subdirs
+    files = [f for f in os.listdir(directory) if not os.path.isdir(f)]
+    for file in files:
+        # We only want to deal with metadata files
+        # If their corresponding data file has come up
+        if file.split(".")[1] == "json":
+                    continue
+        
+        # These fields are set in metadata always.
+        metadata_schema = ["collection", "directory"]
+        metadata = [
+                    {"field": "collection", "value": "file_json"},
+                    {"field": "directory", "value": directory}
+                    ]
+        raw_filename = file.split(".")[0]
+        
+        # JSON Metadata is optional
+        try:
+            extra_md = json.load(open(f"{directory}/{raw_filename}.json"))
+            metadata_schema.extend(extra_md.keys())
+            for key in extra_md.keys():
+                metadata.append({"field": key, "value": extra_md[key]})
+
+        except FileNotFoundError:
+            pass
+
+        """
+        By here, metadata object looks like this:
+        [
+        {'field': 'collection', 'value': 'file_json'},
+        {'field': 'directory', 'value': 'directory_1'},
+        {'field': 'author', 'value': 'Mike Greenberg'},
+        {'field': 'owner', 'value': 'ESPN, a Disney Company'}
+        ]
+        
+        metadata_schema object is:
+        
+        ['collection', 'directory', 'author', 'owner']
+        """
+
+        # Now, we just ingest the file and metadata
+        obj_id = la.ingest(file,
+                         # Levels 
+                         ["low"],
+                         "Description",
+                         metadata_schema=metadata_schema,
+                         metadata=metadata)
